1c61b38fc9ab9b0c27260d2e7334b8e2d1d3f045
AsemAttallah <attallahasem@gmail.com>
2023-03-05 07:08:04 +0000

    y

--- app.js
+++ app.js
@@ -1,25 +1,42 @@
-// "use strict";
-nam=prompt("What's your name?");
-x=prompt("Are you male of female?");
-if((y=prompt("What's your Age?"))>0){
-        if((confirm("Continue to welcoming message ?"))==true){
-            if(x == 'male'){
-                alert("Hello Mr. "+ nam);} 
-            if (x == 'female'){
-                    alert("Hello Ms. "+ nam);}
-            if((x != 'female')&&(x!= 'male')){
-                    alert("Hello "+ nam);
-            }
-                
-            
-            }
+ "use strict";
+let info=[];
+function place(place){
+   if (place=="yes"){
+            info.push("yes");
+        }
+    if (place=="no"){
+            info.push("no");
+        }
+    if (place==""){
+            info.push("Invalid");
+        }
+}
 
+function ownCar(haveCar){
+    if (haveCar=="yes"){
+             info.push("yes");
+         }
+     if (haveCar=="no"){
+             info.push("no");
+         }
+     if (haveCar==""){
+             info.push("Invalid");
+         }
+ }
 
-        }else{
-            alert("please write a real age");} 
-            
-        
-
-
-    
+ function ownLaptop(haveLaptop){
+    if (haveLaptop=="yes"){
+             info.push("yes");
+         }
+     if (haveLaptop=="no"){
+             info.push("no");
+         }
+     if (haveLaptop==""){
+             info.push("Invalid");
+         }
+ }
+place(prompt("Do you live in Jordan?"));
+ownCar(prompt("Do you have a car?"));
+ownLaptop(prompt("Do you have a laptop?"));
+console.log(info);
 
